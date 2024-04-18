393d2238c24d8f1991be99734e70894dd64ee8dc
gaurav01dec <tech.gauravverma@gmail.com>
2024-04-18 16:22:24 +0530

    working

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState,useEffect } from "react"
 // import Winner from "./Winner"
 
 const initialGameBoard = [
@@ -15,16 +15,19 @@ export default function Gameboard({ onSelectSquare, activePlayerSymbol }) {
 
         // const [winner, setWinner] = useState("NA");
         setGameBoard((prevGameBoard) => {
-            debugger;
+            // debugger;
             const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
             updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+
+            winnerCHecker(updatedBoard);
             return updatedBoard;
         })
-
+        
         // console.log(gameBoard);
         onSelectSquare();
         // debugger;
-        winnerCHecker(gameBoard);
+       
+        // winnerCHecker(gameBoard);
 
         // console.log(` winner : ${winner}`);
 
@@ -34,8 +37,8 @@ export default function Gameboard({ onSelectSquare, activePlayerSymbol }) {
 
     }
 
-    const textArea = <h1>........</h1>
-    
+    // const textArea = <h1>........</h1>
+
     function checkifxiswinner(gameBoard) {
         if ((gameBoard[0][0] === 'X' && gameBoard[0][1] === 'X' && gameBoard[0][2] === 'X') || (gameBoard[1][0] === 'X' && gameBoard[1][1] === 'X' && gameBoard[1][2] === 'X') || (gameBoard[2][0] === 'X' && gameBoard[2][1] === 'X' && gameBoard[2][2] === 'X') || (gameBoard[0][0] === 'X' && gameBoard[1][0] === 'X' && gameBoard[2][0] === 'X') || (gameBoard[0][1] === 'X' && gameBoard[1][1] === 'X' && gameBoard[2][1] === 'X') || (gameBoard[0][2] === 'X' && gameBoard[1][2] === 'X' && gameBoard[2][2] === 'X') || (gameBoard[0][0] === 'X' && gameBoard[1][1] === 'X' && gameBoard[2][2] === 'X') || (gameBoard[0][2] === 'X' && gameBoard[1][1] === 'X' && gameBoard[2][0] === 'X')) {
             return true;
@@ -54,11 +57,11 @@ export default function Gameboard({ onSelectSquare, activePlayerSymbol }) {
             setWinner("O");
         }
 
-        if (winner === "X") {
-            textArea = <h1>X is winner</h1>
-        } else if (winner === 'O') {
-            textArea = <h1>O is winner</h1>
-        }
+        // if (winner === "X") {
+        //     textArea = <h1>X is winner</h1>
+        // } else if (winner === 'O') {
+        //     textArea = <h1>O is winner</h1>
+        // }
     }
 
 
@@ -82,7 +85,7 @@ export default function Gameboard({ onSelectSquare, activePlayerSymbol }) {
                 ))}
             </ol>
             {console.log(winner)}
-            {textArea}
+            {winner?winner+' Won':null}
         </>
     )
 };
